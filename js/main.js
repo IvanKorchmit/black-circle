da46cd6458eb503c9209a962267b3fb770697840
Ivan Korchmit <criperadmiral92@gmail.com>
2021-01-30 18:05:38 +0200

    second revision

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let isOver = true;
 let isPlaying = false;
 let animDuration = "0.5s";
 let menu_body = document.querySelector(".menu");
-menu_body.innerHTML = `
+let menu_reserve = `
 <ul>
 <li class="menu-button">Show Menu</li>
 <li class="home"><a href="index.html">Home</a></li>
@@ -14,14 +14,15 @@ menu_body.innerHTML = `
 <li class="summoning"><a href="summoning.html"></a></li>
 </ul>
 `;
+menu_body.innerHTML = menu_reserve;
 let menu = document.querySelector(".menu > ul");
 let main = document.querySelector(".main");
 function openNav() {
     menu_body.style.animationName = "openNav";
     menu_body.style.animationDuration = animDuration;
     menu_body.style.animation;
-    
-    
+
+
     main.style.animationName = "moveMarginLeft";
     main.style.animationDuration = animDuration;
 }
@@ -33,40 +34,71 @@ function closeNav() {
     main.style.animationName = "moveMarginRight";
     main.style.animationDuration = animDuration;
 }
-menu_body.addEventListener("animationend", (e)=>{
-    if(e.animationName == "closeNav" || e.animationName == "openNav") {
+menu_body.addEventListener("animationend", (e) => {
+    if (e.animationName == "closeNav" || e.animationName == "openNav") {
         isPlaying = false;
-        console.log(e.animationName)
-        if(!isOver) {
+        if (!isOver) {
             closeNav();
         }
-        
+
     }
     e.preventDefault();
 });
 
-menu_body.addEventListener("animationstart", (e)=>{
-    if(e.animationName == "closeNav" || e.animationName == "openNav") {
+menu_body.addEventListener("animationstart", (e) => {
+    if (e.animationName == "closeNav" || e.animationName == "openNav") {
         isPlaying = true;
-        console.log(e.animationName)
-        
+
     }
     e.preventDefault();
 });
 
-menu.addEventListener('mouseover',(e)=>{
+menu.addEventListener('mouseover', (e) => {
     isOver = true;
-    if(!isPlaying) {
+    if (!isPlaying) {
         openNav();
     }
     e.preventDefault();
 });
-menu.addEventListener('mouseleave',(e)=>{
+menu.addEventListener('mouseleave', (e) => {
     isOver = false;
-    if(!isPlaying) {
+    if (!isPlaying) {
         closeNav();
     }
     e.preventDefault();
 });
 
 closeNav();
+
+addEventListener("resize", (e) => {
+    adaptMenu();
+    e.preventDefault();
+
+});
+adaptMenu();
+function adaptMenu() {
+    console.log(`Loaded  with ${window.innerWidth} pixels (width)`)
+    if (window.innerWidth <= 768) {
+        if(document.querySelector(".menu-button") != undefined) {
+            document.querySelector(".menu-button").remove();
+            document.querySelector(".bands").remove();
+        }
+    } else {
+        document.querySelector(".menu").innerHTML = menu_reserve;
+        menu = document.querySelector(".menu > ul");
+    }
+    menu.addEventListener('mouseleave', (e) => {
+        isOver = false;
+        if (!isPlaying) {
+            closeNav();
+        }
+        e.preventDefault();
+    });
+    menu.addEventListener('mouseover', (e) => {
+        isOver = true;
+        if (!isPlaying) {
+            openNav();
+        }
+        e.preventDefault();
+    });
+}
